13bf2d3f4dab4606c25de0e149f7cd19513050b8
Matthew <msb2170@gmail.com>
2022-07-20 14:30:44 -0400

    html and css completed

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner msg");
+const list = document.querySelector(".ajax-section .cities");
+
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputVal = input.value
+    const apiKey = 
+})
