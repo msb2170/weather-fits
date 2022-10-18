bb8a1fe9cc091b94e23902e0d79f8f0297dd1439
Matthew <msb2170@gmail.com>
2022-10-18 16:13:40 -0400

    clothing and weather logic changes

--- script.js
+++ script.js
@@ -5,23 +5,40 @@ const list = document.querySelector(".ajax-section .cities");
 const desc = document.querySelector(".fit-rec");
 
 
+function clothingLogic(data) {
+    const {main} = data;
+    if (main.temp <= 0) 
+            //clothes recommendation logic
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>A thick coat, gloves, a hat, and warm pants</h3>`;
+            if (main.temp > 0 && main.temp <= 32) 
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>A warm coat, gloves and a hat</h3>`;
+            if (main.temp > 32 && main.temp <= 50) 
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>A warm coat</h3>`;
+            if (main.temp > 50 && main.temp <= 70) 
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>A light jacket</h3>`;
+            if (main.temp > 70 && main.temp <= 90) 
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>shorts and a t-shirt</h3>`;
+            if (main.temp > 90)
+            desc.innerHTML = `<h1>You Should Wear...</h1>
+            <h3>nothing, if you can get away with it</h3>`
+}
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const inputVal = input.value
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=c6ac1457f388ccda99ace330346b65cc&units=imperial`
-
-    fetch(url)
-        .then(response => response.json())
-        .then( data => {
-            const {main, name, sys, weather} = data
-            const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+function weatherLogic(data) {
+    const {main, name, sys, weather} = data
+    const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
             const li = document.createElement("li");
             li.classList.add("city");
+            li.setAttribute('id', Date())
             const markup = `
             <h2 class="city-name" data-name=${name}, ${sys.country}:>
             <span>${name}</span>
             <sup>${sys.country}</sup>
+            <span class="x-button"><h6>X</h6></span>
             </h2>
             <div class="city-temp">${Math.round(main.temp)}<sup>°F</sup>
             </div>
@@ -32,28 +49,43 @@ form.addEventListener("submit", (e) => {
             `;
             li.innerHTML = markup
             list.appendChild(li)
-            if (main.temp <= 0) {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>A thick coat, gloves, a hat, and warm pants</h3>`
-        } else if (main.temp > 0 && main.temp <= 32) {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>A warm coat, gloves and a hat</h3>`
-        } else if (main.temp > 32 && main.temp <= 50) {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>A warm coat</h3>`
-        } else if (main.temp > 50 && main.temp <= 70) {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>A light jacket</h3>`
-        } else if (main.temp > 70 && main.temp <= 90) {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>shorts and a t-shirt</h3>`
-        } else {
-            desc.innerHTML = `<h1>You Should Wear...</h1>
-            <h3>nothing, if you can get away with it</h3>`
+            li.addEventListener("click", () => {
+                clothingLogic(data)
+            })
+            const xBtn = li.querySelector(".x-button");
+            xBtn.addEventListener("click", (e) => {
+                const element = e.currentTarget.parentElement.parentElement;
+                element.remove();
+            });
+            
+}
+
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputVal = input.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=&units=imperial`
+
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        }) 
+        .then( data => {
+            clothingLogic(data)
+            weatherLogic(data)
+            input.value = '';
         }
-        })
+        )
         .catch((err) => {
-            msg.textContent(err)
+            alert('Please enter a valid city');
+            console.log(err);
+            input.value = '';
         })
+        
 });
 
+form.addEventListener("reset", () => {
+    desc.innerHTML = ``;
+    list.innerHTML = ``;
+});
